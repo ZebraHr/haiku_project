ff64cced425c221f8cb3f54eabd5ad5ebb934f7b
Anna Pobedonostseva <zebra9999@rambler.ru>
2024-02-16 18:42:11 +0300

    Добавлена модель поста, тестовый вывод поста на главную страницу

--- haiku/posts/models.py
+++ haiku/posts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField(
+        verbose_name='Текст хайку',
+        help_text='Введите текст поста',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор поста'
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+        )
+    haiku_author = models.CharField(
+        max_length=30,
+        verbose_name='Автор хайку',
+        blank=True
+    )
+    image = models.ImageField(
+        'Картинка',
+        upload_to='posts/',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text
--- haiku/posts/views.py
+++ haiku/posts/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
+
 
 def index(request):
     """Главная страница."""
-    return render(request, 'posts/index.html')
+    posts = Post.objects.all()[:10]
+    # page_obj = pagination(request, posts)
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def profile(request, username):
